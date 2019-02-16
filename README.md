7f5d690571df0628f217990aac65ec79cf610d64
Zsolt Sz. Sztupák <mail@sztupy.hu>
2019-02-16 04:41:55 +0000

    Reorganise project - uses the new JS based generator framework

--- README.md
+++ README.md
@@ -1,6 +1,59 @@
-Scripts I used to find the point furhtest away in Hungary from a street named Kossuth
+Determine furthest points away in a country for a specific set of points
+========================================================================
 
-To generate `streets.csv` use:
+Based on a set of locations (nodes) these scripts will try to figure out which points are the furthest away from these locations in a country.
+
+It uses the following algorithm:
+
+First it tries to find locations, which are potentially far away from our input set:
+
+1. It generates the Voronoi-graph based on the input set of locations
+2. It checks each of the generated vertices whether they are within the country's boundaries or not. Any point that's in the boundary gets added to the potential list of candidates
+3. Each of the vertices of the boundary are also added to the potential candidates list
+4. Finally any point on the boundary that is bisected by a Voronoi-edge is also added to the candidate list
+
+The vertices of the Voronoi-graph are good candidates as they are the furthest point away from their neighbouring locations. However some of these points can be outside of the country's boundaries. To make sure they are also accounted for both the vertices in the country's boundaries are added to the list, as well as any place where the boundary was intersected by a voroni-edge. These are the points where distances are at a local maximum, so it's worth checking.
+
+Once we have a set of potential locations, we simply go through them, find the nearest node to them, and check it's distance. Then we sort these nodes according to how large it's distance is.
+
+To make the result not too clustered, we also remove points that are too close together at the end. This makes sure that remote border locations, which have a large amount of points in the search set will not be over-represented in the result, as we are interested in more distinct locations anyway.
+
+Once we have the end values we render them to a map as well.
+
+Usage
+-----
+
+The app requires Node.JS to be installed, and the proper modules downloaded using `npm install`.
+
+Once the setup is done change the constants at the start of `generate_data.js` to match the input and output filenames, then simply run `./generate_data.js`
+
+Some useful config values to change:
+
+* `INPUT_FILE_NAME`: The input file containing the set of points we wish to avoid. It's a JSON array of `[lon,lat]` pairs preferably in WGS84.
+* `BOUNDARY_FILE_NAME`: The file containig the boundary of the country. It should only contain one large polygon, countries which are made out of multiple polygons are not yet tested, and can likely lead to some errorneous results.
+* `OUTPUT_FILE_NAME`: The file where the rendered image should be saved in PNG format.
+* `IMAGE_WIDTH`: Set this to the preferred image width size. The height of the image will be calculated automatically to keep the aspect relations.
+* `NUMBER_OF_CLUSTERS`: The amount of points we wish to obtain at the end.
+* `CLUSTERING_MINUMUM_DISTANCE`: Sets how far away two points need to be (in km) in order to be considered for inclusion.
+
+You can also change the styling on the output by changing any of the other values
+
+Examples
+--------
+
+Some examples:
+
+### Points furthest away in Hungary from a street named "Kossuth"
+
+Large resolution map:
+![Kossuth large](https://raw.githubusercontent.com/sztupy/kossuth-map/master/kossuth.png)
+
+Small resolution map:
+![Kossuth small](https://raw.githubusercontent.com/sztupy/kossuth-map/master/kossuth_small.png)
+
+The dataset is called `kossuth_points.json`:
+
+To generate `kossuth_points.json` use:
 
 1. Download Hungary.osm.pbf from http://data.osm-hr.org/hungary/
 
@@ -16,10 +69,14 @@ osmosis –read-pbf-fast hungary.osm.pbf file=“hungary.osm.pbf” –way-key k
 osmconvert hungary2.osm –all-to-nodes –csv=“@id @lon @lat name” –csv-headline | grep -i kossuth > streets.csv
 ```
 
-Next to generate the vornoi diagram I used `rubyvor`. Dependent on the scaling used it will generate a more-or less nice SVG, with the relevant data. You can find a generated svg called `dia.dvg`, and an export to png called `dia.png`:
+Finally I converted the CSV to JSON using some sed scripts.
 
-![Voronoi of Hungary](https://raw.githubusercontent.com/sztupy/kossuth-map/master/dia.png)
+### Points furthest away in Hungary from any sports complex ==
 
-And one with a crude overlay of the borders of Hungary:
+Large resolution map:
+![Kossuth large](https://raw.githubusercontent.com/sztupy/kossuth-map/master/stadionok.png)
 
-![Voronoi of Hungary](https://raw.githubusercontent.com/sztupy/kossuth-map/master/hungary-overlay.png)
+Small resolution map:
+![Kossuth small](https://raw.githubusercontent.com/sztupy/kossuth-map/master/stadionok_small.png)
+
+The dataset is from a list I gathered for another project called Magyar Stadionok: https://github.com/sztupy/magyar-stadionok
---
+++ generate_data.js
@@ -0,0 +1,357 @@
+#!/usr/bin/env node
+
+console.log('Preparing data');
+
+// configuration
+const INPUT_FILE_NAME = 'kossuth_points.json';
+const BOUNDARY_FILE_NAME = 'hungary.json';
+const OUTPUT_FILE_NAME = 'kossuth.png';
+
+const IMAGE_WIDTH = 10000; // px
+
+const NUMBER_OF_CLUSTERS = 10;
+const CLUSTERING_MINIMUM_DISTANCE = 15; //km
+
+const NODE_FILL_COLOR = "rgba(80,80,80, 0.25)";
+const NODE_STROKE_COLOR = "rgba(0,0,0, 0.5)";
+const NODE_STROKE_WIDTH = 0.5;
+const NODE_RADIUS = IMAGE_WIDTH / 1000;
+
+const VORONOI_CELL_STROKE_COLOR = "#333333";
+const VORONOI_CELL_STROKE_WIDTH = 0.5;
+
+const BOUNDARY_STROKE_COLOR = "#000000";
+const BOUNDARY_STROKE_WIDTH = IMAGE_WIDTH / 2000;
+
+const VORONOI_NODE_ACTIVE_FILL_COLOR = "#FF5555";
+const VORONOI_NODE_INACTIVE_FILL_COLOR = "#555555";
+const VORONOI_NODE_ACTIVE_STROKE_COLOR = "#FF0000";
+const VORONOI_NODE_INACTIVE_STROKE_COLOR = "#000000";
+const VORONOI_NODE_STROKE_WIDTH = 0.5;
+const VORONOI_NODE_RADIUS = IMAGE_WIDTH / 2000;
+
+const BOUNDARY_VERTEX_FILL_COLOR = "#55FF55";
+const BOUNDARY_VERTEX_STROKE_COLOR = "#00FF00";
+const BOUNDARY_VERTEX_STROKE_WIDTH = 0.5;
+const BOUNDARY_VERTEX_RADIUS = IMAGE_WIDTH / 2000;
+
+const BOUNDARY_INTERSECTION_FILL_COLOR = "#99FF99";
+const BOUNDARY_INTERSECTION_STROKE_COLOR = "#00FF00";
+const BOUNDARY_INTERSECTION_STROKE_WIDTH = 0.5;
+const BOUNDARY_INTERSECTION_RADIUS = IMAGE_WIDTH / 2000;
+
+const FOUND_NODE_FILL_COLOR = "rgba(255,0,0,0.1)";
+const FOUND_NODE_STROKE_COLOR = "rgba(255,0,0,0.5)";
+const FOUND_NODE_ARROW_STROKE_COLOR = "rgba(0,0,0,0.5)";
+const FOUND_NODE_ARROW_STROKE_WIDTH = 6;
+const FOUND_NODE_STROKE_WIDTH = 3;
+const FOUND_NODE_RADIUS = IMAGE_WIDTH / 100;
+
+const FOUND_NODE_FONT = "60px Arial";
+const FOUND_NODE_FONT_FILL_COLOR = "#000000";
+const FOUND_NODE_FONT_STROKE_COLOR = "#FFFFFF";
+const FOUND_NODE_FONT_STROKE_WIDTH = 10;
+const FOUND_NODE_TEXT_HEIGHT = 200; // No way to get this automated using HTML5 Canvas
+
+//requires
+const { Delaunay } = require('d3-delaunay');
+const fs = require('fs');
+const { createCanvas } = require('canvas');
+const turf = require('@turf/turf');
+const LatLon = require('geodesy').LatLonEllipsoidal;
+
+const points = JSON.parse(fs.readFileSync(INPUT_FILE_NAME));
+const boundary = JSON.parse(fs.readFileSync(BOUNDARY_FILE_NAME));
+
+const boundaryTurf = turf.polygon([boundary]);
+
+const delaunay = Delaunay.from(points);
+
+// figure out the boundaries
+var minX = points[0][0];
+var maxX = points[0][0];
+var minY = points[0][1];
+var maxY = points[0][1];
+
+for (let p of points) {
+    if (p[0] < minX) { minX = p[0]; }
+    if (p[0] > maxX) { maxX = p[0]; }
+    if (p[1] < minY) { minY = p[1]; }
+    if (p[1] > maxY) { maxY = p[1]; }
+}
+
+for (let p of boundary) {
+    if (p[0] < minX) { minX = p[0]; }
+    if (p[0] > maxX) { maxX = p[0]; }
+    if (p[1] < minY) { minY = p[1]; }
+    if (p[1] > maxY) { maxY = p[1]; }
+}
+
+// add some leeway around the edges
+let d = (maxX-minX)/50;
+minX -= d; maxX += d;
+d = (maxY-minY)/50;
+minY -= d; maxY += d;
+
+// set up the image
+const WIDTH = maxX-minX;
+const HEIGHT = maxY-minY;
+
+const SIZE_X = IMAGE_WIDTH;
+const SIZE_Y = Math.round(SIZE_X / WIDTH * HEIGHT);
+
+const canvas = createCanvas(SIZE_X, SIZE_Y);
+const ctx = canvas.getContext('2d');
+
+console.log('Generating Voronoi Diagram');
+// calculate the data
+const voronoi = delaunay.voronoi([minX, minY, maxX, maxY]);
+
+// gets the X coordinate on the image from a longitude
+function getX(lon) {
+    return (lon - minX)/WIDTH * SIZE_X;
+}
+
+// gets the Y coordinate on the image from a latitude
+function getY(lat) {
+    return SIZE_Y - (lat - minY)/HEIGHT * SIZE_Y;
+}
+
+// draws a polygon
+function drawPoly(ctx, poly) {
+    ctx.beginPath();
+    const lastPoly = poly[poly.length-1];
+    ctx.moveTo(getX(lastPoly[0]), getY(lastPoly[1]));
+
+    for (let i=0; i<poly.length; i++) {
+        ctx.lineTo(getX(poly[i][0]), getY(poly[i][1]));
+    }
+    ctx.stroke();
+}
+
+// draws a circle
+function drawCircle(ctx, point, radius) {
+    ctx.beginPath();
+    ctx.arc(getX(point[0]), getY(point[1]), radius, 0, Math.PI*2);
+    ctx.fill();
+    ctx.stroke();
+}
+
+// converts a point to a string to be used in hashes
+function pointToKey(point) {
+    return point[0]+' '+point[1];
+}
+
+// converts the string back to a point
+function keyToPoint(key) {
+    let data = key.split(" ");
+    return [parseFloat(data[0]), parseFloat(data[1])];
+}
+
+console.log("Checking boundary conditions");
+// gets all of the interesting points from the voronoi cells - these are the vertices of the cells
+var locations = new Map();
+for (let poly of voronoi.cellPolygons()) {
+    for (let i=0; i<poly.length; i++) {
+        locations.set(pointToKey(poly[i]), false);
+    }
+}
+
+// check which of these points lie within our boundary, and which ones are outside
+var locationsPoints = [];
+for (let [key, _] of locations) {
+    locationsPoints.push(keyToPoint(key));
+}
+var locationsTurf = turf.points(locationsPoints);
+var pointsInBoundary = turf.pointsWithinPolygon(locationsTurf, boundaryTurf);
+
+turf.featureEach(pointsInBoundary, (currentFeature, _) => {
+    let point = turf.getCoord(currentFeature);
+    locations.set(pointToKey(point), true);
+});
+
+console.log("Finding locations on boundary");
+var intersectPoints = [];
+// collects all points which intersect our boundary
+for (let poly of voronoi.cellPolygons()) {
+    for (let i=0; i<poly.length; i++) {
+        if (locations.get(pointToKey(poly[i])) != locations.get(pointToKey(poly[(i + 1) % poly.length]))) {
+            let lineTurf = turf.lineString([poly[i], poly[(i + 1) % poly.length]]);
+            let intersects = turf.lineIntersect(lineTurf, boundaryTurf);
+            turf.featureEach(intersects, (currentFeature, _) => {
+                let point = turf.getCoord(currentFeature);
+                intersectPoints.push(point);
+            });
+        }
+    }
+};
+
+// collect all points that are potentially a furthest one
+var potentialPoints = [];
+
+// draw the points from the voronoi dataset
+for (let [key, value] of locations) {
+    let point = keyToPoint(key);
+    if (value) {
+        potentialPoints.push(point);
+    }
+}
+
+// draw the vertices on the boundary
+for (let point of boundary) {
+    potentialPoints.push(point);
+}
+
+for (let point of intersectPoints) {
+    potentialPoints.push(point);
+}
+
+console.log("Obtaining distance data");
+
+// find the furthest away node from each of our selected points
+var distanceMap = [];
+for (let point of potentialPoints) {
+    let otherPoint = points[delaunay.find(point[0],point[1])];
+    let p1 = new LatLon(point[0], point[1]);
+    let p2 = new LatLon(otherPoint[0], otherPoint[1]);
+    let d = p1.distanceTo(p2);
+    distanceMap.push([d, {
+        point: point,
+        destination: otherPoint
+    }]);
+}
+
+// sort in order of decreasing distance
+distanceMap.sort((a,b) => b[0] - a[0]);
+
+// cluster points - only keep one in each cluster
+console.log("Clustering points");
+var distanceCluster = [];
+var index = 0;
+
+while (distanceCluster.length<NUMBER_OF_CLUSTERS || index > distanceMap.length) {
+    let data = distanceMap[index];
+    let minDist = Infinity;
+    for (let i = 0; i < distanceCluster.length; i++) {
+        let p1 = new LatLon(data[1].point[0], data[1].point[1]);
+        let p2 = new LatLon(distanceCluster[i][1].point[0], distanceCluster[i][1].point[1]);
+        let d = p1.distanceTo(p2);
+        if (d < minDist) {
+            minDist = d;
+        }
+    }
+    if (minDist > CLUSTERING_MINIMUM_DISTANCE * 1000) {
+        distanceCluster.push(data);
+    }
+    index++;
+}
+
+console.log("Generating image");
+
+// draw the nodes
+for (let point of points) {
+    ctx.fillStyle = NODE_FILL_COLOR;
+    ctx.strokeStyle = NODE_STROKE_COLOR;
+    ctx.lineWidth = NODE_STROKE_WIDTH;
+    drawCircle(ctx, point, NODE_RADIUS);
+}
+
+// draw all cells, and collect the cell boundaries
+for (let poly of voronoi.cellPolygons()) {
+    ctx.strokeStyle = VORONOI_CELL_STROKE_COLOR;
+    ctx.lineWidth = VORONOI_CELL_STROKE_WIDTH;
+    drawPoly(ctx, poly);
+};
+
+// draw the boundary
+ctx.strokeStyle = BOUNDARY_STROKE_COLOR;
+ctx.lineWidth = BOUNDARY_STROKE_WIDTH;
+drawPoly(ctx, boundary);
+
+// draw the points from the voronoi dataset
+for (let [key, value] of locations) {
+    let point = keyToPoint(key);
+    ctx.fillStyle = value ? VORONOI_NODE_ACTIVE_FILL_COLOR : VORONOI_NODE_INACTIVE_FILL_COLOR;
+    ctx.strokeStyle = value ? VORONOI_NODE_ACTIVE_STROKE_COLOR : VORONOI_NODE_INACTIVE_STROKE_COLOR;
+    ctx.lineWidth = VORONOI_NODE_STROKE_WIDTH;
+    drawCircle(ctx, point, VORONOI_NODE_RADIUS);
+}
+
+// draw the vertices on the boundary
+for (let point of boundary) {
+    ctx.fillStyle = BOUNDARY_VERTEX_FILL_COLOR;
+    ctx.strokeStyle = BOUNDARY_VERTEX_STROKE_COLOR;
+    ctx.lineWidth = BOUNDARY_VERTEX_STROKE_WIDTH;
+    drawCircle(ctx, point, BOUNDARY_VERTEX_RADIUS);
+}
+
+// draw the intersections of the voronoi points with the boundary
+for (let point of intersectPoints) {
+    ctx.fillStyle = BOUNDARY_INTERSECTION_FILL_COLOR;
+    ctx.strokeStyle = BOUNDARY_INTERSECTION_STROKE_COLOR;
+    ctx.lineWidth = BOUNDARY_INTERSECTION_STROKE_WIDTH;
+    drawCircle(ctx, point, BOUNDARY_INTERSECTION_RADIUS);
+}
+
+var textBoxes = [];
+
+for (let i = 0; i < distanceCluster.length; i++) {
+    ctx.fillStyle = FOUND_NODE_FILL_COLOR;
+    ctx.strokeStyle = FOUND_NODE_STROKE_COLOR;
+    ctx.lineWidth = FOUND_NODE_STROKE_WIDTH;
+    let data = distanceCluster[i][1];
+    drawCircle(ctx, data.point, FOUND_NODE_RADIUS);
+    console.log("Point from "+data.point[1]+","+data.point[0]+" to "+data.destination[1]+","+data.destination[0]+" is "+distanceCluster[i][0]/1000+"km");
+
+    ctx.font = FOUND_NODE_FONT;
+    ctx.fillStyle = FOUND_NODE_FONT_FILL_COLOR;
+    ctx.strokeStyle = FOUND_NODE_FONT_STROKE_COLOR;
+    ctx.lineWidth = FOUND_NODE_FONT_STROKE_WIDTH;
+    ctx.textBaseline = "top";
+    ctx.textAlign = "start";
+
+    let textX = getX(data.point[0]) + FOUND_NODE_RADIUS;
+    let textY = getY(data.point[1]) + FOUND_NODE_RADIUS;
+    let text = data.point[1]+"\n"+data.point[0]+"\n"+distanceCluster[i][0]/1000+"km";
+    let textWidth = ctx.measureText(text).width;
+
+    // crappy algorithm to make the textboxes not overlap
+    let okay = false;
+    let tries = 50;
+    while (!okay && tries > 0)  {
+        okay = true;
+        for (let ii = 0; ii < textBoxes.length; ii++) {
+            if (! (textBoxes[ii][0] > textX + textWidth ||
+                   textBoxes[ii][2] < textX ||
+                   textBoxes[ii][1] > textY + FOUND_NODE_TEXT_HEIGHT ||
+                   textBoxes[ii][3] < textY
+                  )
+               ) {
+                textX += Math.random() * FOUND_NODE_TEXT_HEIGHT - FOUND_NODE_TEXT_HEIGHT/2;
+                textY += Math.random() * FOUND_NODE_TEXT_HEIGHT - FOUND_NODE_TEXT_HEIGHT/2;
+                okay = false;
+            }
+        }
+        tries--;
+    }
+
+    ctx.strokeText(text, textX, textY);
+    ctx.fillText(text, textX, textY);
+
+    ctx.strokeStyle = FOUND_NODE_ARROW_STROKE_COLOR;
+    ctx.lineWidth = FOUND_NODE_ARROW_STROKE_WIDTH;
+    ctx.beginPath();
+    ctx.moveTo(textX, textY + FOUND_NODE_TEXT_HEIGHT/2);
+    ctx.lineTo(getX(data.point[0]), getY(data.point[1]));
+    ctx.stroke();
+
+    textBoxes.push([textX, textY, textX+textWidth, textY+FOUND_NODE_TEXT_HEIGHT]);
+}
+
+console.log("Saving image");
+
+// save image to file
+var buf = canvas.toBuffer();
+fs.writeFileSync(OUTPUT_FILE_NAME, buf);
+
+console.log("Done");
